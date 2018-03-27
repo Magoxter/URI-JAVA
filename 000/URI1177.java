e7af239c4aa9b1129bca6d336f3cbaf7d09a30b4
Will Xavier <will.lbx@hotmail.com>
2018-03-27 10:23:41 -0400

    Preenchimento de Vetor II
    URI - 1177

---
+++ 000/URI1177.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class URI1177 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		int i = 0;
+
+		while (i < 1000) {
+			System.out.printf("N[%d] = %d\n", i, i %n);
+		++i;
+		}
+	}
+}
